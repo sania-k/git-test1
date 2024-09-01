2d30c981be0afca46f7d168b1e487eb3cc381d4f
Sania <skhan59@uncc.edu>
2024-09-01 01:26:31 -0400

    more calculations :)

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println(9 * 11);
+        System.out.println(8 * 11);
+        System.out.println(7 * 11);
     }
 }
